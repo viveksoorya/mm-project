d73e7e4e59aa3b358436d0fe22364a05a85cbb2e
uni <wewakesoorya@gmail.com>
2025-08-08 09:26:24 +0530

    Ran xtb goat for amino acids A through L, extracted energies of top 6 conformers and plotted scatter plots. Refactored files to reflect demarcation of responsibility.

---
+++ aishani/script-for-extracting-conformers.sh
@@ -0,0 +1,16 @@
+# extract the number from the first line in the finalensemble
+# go to line 3
+# cat the next n lines to a file k in 0 1 2 3 4 5
+# after each cat skip 2 lines
+
+for xaa in a c d e f g h i k l; do
+	file=${xaa}/${xaa}.finalensemble.xyz
+	n=$(head -n 1 "$file" | awk '{print $1}')
+	
+	startline=3
+	for conformer in 0 1 2 3 4 5; do
+		endline=$((startline + n - 1))
+		sed -n "${startline},${endline}p" "$file" > "$xaa/$conformer"
+		startline=$((endline+3))
+	done
+done
