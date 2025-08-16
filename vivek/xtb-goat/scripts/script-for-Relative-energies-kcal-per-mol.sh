ff84fd46fca8987ed31e6143eae7f4195ca99c4f
vivek <wewakesoorya@gmail.com>
2025-08-16 15:22:36 +0530

    A and L amino acid structures' orca runs for xtb-goat redone, due to incorrect structure. Relative energies in kcal per mol extracted. Scripts were altered a bit with regards to file renaming.

--- vivek/xtb-goat/scripts/script-for-Relative-energies-kcal-per-mol.sh
+++ vivek/xtb-goat/scripts/script-for-Relative-energies-kcal-per-mol.sh
@@ -1,3 +1,3 @@
-for j in d g k; do
-	awk '{print $1 * 627.509}' "${j}/${j^^}.Relative-ENERGIES" > "${j}/${j^^}.Relative-ENERGIES-kcal-per-mol"
+for j in a l; do
+	awk '{print $1 * 627.509}' "${j}/Relative-ENERGIES" > "${j}/Relative-ENERGIES-kcal-per-mol"
 done
--- vivek/xtb-goat/scripts/script-for-Relative-energies.sh
+++ vivek/xtb-goat/scripts/script-for-Relative-energies.sh
@@ -1,3 +1,3 @@
-for j in d g k; do
-	awk 'NR==1 {tmp=$1} {print $1 - tmp}' ${j}/${j^^}.ENERGIES > ${j}/${j^^}.Relative-ENERGIES 
+for j in l; do
+	awk 'NR==1 {tmp=$1} {print $1 - tmp}' ${j}/ENERGIES > ${j}/Relative-ENERGIES 
 done
