ff164396c92b5f940ed19850369e9a56d32d7454
vivek <wewakesoorya@gmail.com>
2025-08-16 15:22:36 +0530

    A and L amino acid structures' orca runs for xtb-goat redone, due to incorrect structure. Relative energies in kcal per mol extracted. Scripts were altered a bit with regards to file renaming.
    Plotted the Relative-ENERGIES-kcal-mol-1-dashes-AMINOACIDS-A-through-L.png plot from laptop using molecular-modelling conda env's python binary applied to a python script located in the scripts folder of xtb-goat.
    
    Finished moving a and l of MP2 nad B97-D of xtb-goat to
    a-wrong-structure and l-wrong-structure.
    
    Re-ran orca runs of all conformers of alanine and leucine using MP2 and
    B97-D functionals. Extracted their Relative Energies in kcal per mol.
    
    Plots of energies calculated using MP2 and B97-D functionals for
    comparison, for alanine and leucine are pending.

--- vivek/xtb-goat/scripts/script-for-Relative-energies.sh
+++ vivek/xtb-goat/scripts/script-for-Relative-energies.sh
@@ -1,3 +1,3 @@
-for j in l; do
+for j in a l; do
 	awk 'NR==1 {tmp=$1} {print $1 - tmp}' ${j}/ENERGIES > ${j}/Relative-ENERGIES 
 done
--- vivek/xtb-goat/scripts/script-to-run-MP2.sh
+++ vivek/xtb-goat/scripts/script-to-run-MP2.sh
@@ -1,12 +1,12 @@
-for j in d g k ; do
+for j in l ; do
 	for dir in xtb-goat/${j}/${j^^}_CONF; do
 		mkdir MP2/${j}/
 		for conformer in 0 1 2 3 4 5; do
 			mkdir MP2/${j}/${conformer}
-			echo -e "! MP2 6-31G** \n\n* xyzfile 0 1 /home/vivek/mm-project/vivek/${dir}/${conformer}.xyz" > /home/vivek/mm-project/vivek/MP2/${j}/${conformer}/${conformer}.inp
-			cd /home/vivek/mm-project/vivek/MP2/${j}/${conformer}/
+			echo -e "! MP2 6-31G** \n\n* xyzfile 0 1 /home/vivek/mm-project/vivek/xtb-goat/${dir}/${conformer}.xyz" > /home/vivek/mm-project/vivek/xtb-goat/MP2/${j}/${conformer}/${conformer}.inp
+			cd /home/vivek/mm-project/vivek/xtb-goat/MP2/${j}/${conformer}/
 			orca ${conformer}.inp > ${conformer}.out
-			cd /home/vivek/mm-project/vivek/ 
+			cd /home/vivek/mm-project/vivek/xtb-goat/ 
 		done
 	done
 done
