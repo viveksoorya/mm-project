a65b42e8fe93ff1b3b1ee21e45cc91cf83cb9945
uni <wewakesoorya@gmail.com>
2025-08-19 15:23:50 +0530

    MAD values and Deviations redone; Earlier I used energies in hartrees rather than relative energies in kcal per mol.

--- vivek/xtb-goat/scripts/Deviations-for-MP2.sh
+++ vivek/xtb-goat/scripts/Deviations-for-MP2.sh
@@ -2,7 +2,7 @@ dir=/home/uni/mm-project/vivek/xtb-goat/
 cd $dir
 for j in a c d e f g h i k l; do
 	for conformer in 0 1 2 3 4 5; do
-		diff=$(echo "$(awk -v line=$((conformer+1)) 'NR==line {print $1}' "$dir/xtb-goat/$j/ENERGIES") - $(awk -v line=$((conformer+1)) 'NR==line {print $1}' "$dir/MP2/$j/ENERGIES")" | bc)
+		diff=$(echo "$(awk -v line=$((conformer+1)) 'NR==line {print $1}' "$dir/xtb-goat/$j/Relative-ENERGIES-kcal-per-mol") - $(awk -v line=$((conformer+1)) 'NR==line {print $1}' "$dir/MP2/$j/Relative-ENERGIES-kcal-per-mol")" | bc)
 		abs_diff=$(echo "$diff" | awk '{if($1<0) print -$1; else print $1}')
 		echo $abs_diff >> "MP2/$j/DEVIATIONS-OF-XTB-GOAT"
 	done
